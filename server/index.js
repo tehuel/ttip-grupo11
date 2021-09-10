5c5c2ca9ad6d5dc821e3d0370d13f0a293a31bf7
Tehuel <torresbaldi.tehuel@gmail.com>
2021-09-10 19:21:54 -0300

    move express app to its own module

--- server/index.js
+++ server/index.js
@@ -1,22 +1,5 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const config = require("./config.js");
-const IngredientRoute = require("./routes/ingredient.route");
-
-const app = express();
-const port = 3000;
-
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.get("/", (_req, res) => {
-  res.status(200).json({
-    message: "OK",
-  });
-});
-app.use("/ingredient", IngredientRoute);
+const app = require("./app");
 
 app.listen(config.PORT, config.HOST, function () {
   console.log(`App listening on http://${config.HOST}:${config.PORT}`);
