cdd3757daf21b37502e2737b1f6f5fde813fd6a0
Tehuel <torresbaldi.tehuel@gmail.com>
2021-10-15 22:48:21 -0300

    format tags response

--- client/service/tag.js
+++ client/service/tag.js
@@ -1,14 +1,14 @@
-module.exports = {
-    getTags: async (axios) => {
-      const tagsResponse = await axios.$get('/tags')
-      return tagsResponse.data.map((tag) => {
-        // format tags
-        return {
-          id: tag._id,
-          name: tag.name,
-          createdAt: new Date(tag.createdAt),
-        }
-      })
-    },
+const formatTag = (IngredientResponse) => {
+  return {
+    id: IngredientResponse._id,
+    name: IngredientResponse.name,
+    createdAt: new Date(IngredientResponse.createdAt),
   }
-  
+}
+
+module.exports = {
+  getTags: async (axios) => {
+    const tagsResponse = await axios.$get('/tags')
+    return tagsResponse.data.map(formatTag)
+  },
+}
