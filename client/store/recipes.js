cbc1283be29ae128ebb7b6abba7c986847e80fde
Tehuel <torresbaldi.tehuel@gmail.com>
2021-10-24 09:01:29 -0300

    elimino manejo de loading de vuex

--- client/store/recipes.js
+++ client/store/recipes.js
@@ -1,26 +1,19 @@
 const RecipeService = require('../service/recipe')
 
 export const state = () => ({
-  loading: false,
   latest: [],
 })
 
 export const actions = {
   async getLatestRecipes({ commit, state }) {
-    commit('setLoading', true)
     const foundRecipes = await RecipeService.getLatestRecipes(this.$axios, {
       skip: state.latest.length,
     })
     commit('setLatest', foundRecipes)
-    setTimeout(() => commit('setLoading', false), 1000)
   },
 }
 
 export const mutations = {
-  setLoading(state, loadingState) {
-    state.loading = loadingState
-  },
-
   setLatest(state, recipesList) {
     state.latest = [...state.latest, ...recipesList]
   },
