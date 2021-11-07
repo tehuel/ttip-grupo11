cdf3776809586dc642c3dd769084e585d844aaae
Tehuel <torresbaldi.tehuel@gmail.com>
2021-11-07 16:33:22 -0300

    refactor pagina de about

--- client/pages/about.vue
+++ client/pages/about.vue
@@ -1,29 +1,26 @@
 <template>
-  <div>
-    <div class="container my-5">
-      <h1>Acerca de Recetaris</h1>
-      <p>
-        Aplicación desarrollada para la materia "Taller de Trabajo de Inserción
-        Profesional" de la UNQ.
-      </p>
-      <hr />
-      <h2 class="h3">Licencias</h2>
-      <p>
-        Photo:
-        <a
-          href="https://foto.wuestenigel.com/food-background-with-ingredients-for-cooking-spaghetti-or-noodles/"
-          target="_blank"
-          >Food background with ingredients for cooking spaghetti or noodles</a
-        >
-        by
-        <a href="https://linktr.ee/wuestenigel" target="_blank">Marco Verch</a>
-        under
-        <a href="https://creativecommons.org/licenses/by/2.0/" target="_blank"
-          >Creative Commons 2.0</a
-        >
-      </p>
-    </div>
-  </div>
+  <ContainersSection title="Acerca de Recetaris">
+    <p>
+      Aplicación desarrollada para la materia "Taller de Trabajo de Inserción
+      Profesional" de la UNQ.
+    </p>
+    <hr />
+    <h2 class="h3">Licencias</h2>
+    <p>
+      Photo:
+      <a
+        href="https://foto.wuestenigel.com/food-background-with-ingredients-for-cooking-spaghetti-or-noodles/"
+        target="_blank"
+        >Food background with ingredients for cooking spaghetti or noodles</a
+      >
+      by
+      <a href="https://linktr.ee/wuestenigel" target="_blank">Marco Verch</a>
+      under
+      <a href="https://creativecommons.org/licenses/by/2.0/" target="_blank"
+        >Creative Commons 2.0</a
+      >
+    </p>
+  </ContainersSection>
 </template>
 
 <script>
