2c0b7833d753641b3ba8abc0c35da76ba6984cbf
Tehuel <torresbaldi.tehuel@gmail.com>
2021-11-07 15:52:05 -0300

    refactor

---
+++ client/components/containers/Form.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container my-5">
+    <div class="row justify-content-center">
+      <div class="col col-md-10 col-lg-8">
+        <div class="card shadow-lg">
+          <div class="card-body">
+            <h1 class="h2 text-center">{{ title }}</h1>
+            <slot></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormContainer',
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
--- client/pages/register.vue
+++ client/pages/register.vue
@@ -1,72 +1,56 @@
 <template>
-  <div class="page">
-    <div class="container my-5">
-      <div class="row justify-content-center">
-        <div class="col col-md-10 col-lg-8">
-          <div class="card shadow-lg">
-            <div class="card-body">
-              <h1 class="h2 text-center">Registrar Nuevo Usuario</h1>
-              <template v-if="error">
-                <b-alert show variant="danger" class="my-5">
-                  <p>Hubo un error en el registro.</p>
-                  <hr />
-                  <b-button
-                    variant="danger"
-                    block
-                    @click.stop.prevent="resetError"
-                  >
-                    Reintentar
-                  </b-button>
-                </b-alert>
-              </template>
-              <template v-else>
-                <p>
-                  Completá el formulario para registrar un nuevo usuario y
-                  acceder a todas las funciones del sitio.
-                </p>
-                <b-form
-                  class="my-5"
-                  @submit.stop.prevent="onSubmitRegisterForm"
-                >
-                  <b-form-group
-                    id="input-group-email"
-                    label="Email:"
-                    label-for="input-email"
-                  >
-                    <b-form-input
-                      id="input-email"
-                      v-model="form.email"
-                      type="email"
-                      required
-                    ></b-form-input>
-                  </b-form-group>
+  <div id="register-page">
+    <ContainersForm title="Registrar Nuevo Usuario">
+      <template v-if="error">
+        <b-alert show variant="danger" class="my-5">
+          <p>Hubo un error en el registro.</p>
+          <hr />
+          <b-button variant="danger" block @click.stop.prevent="resetError">
+            Reintentar
+          </b-button>
+        </b-alert>
+      </template>
+      <template v-else>
+        <p>
+          Completá el formulario para registrar un nuevo usuario y acceder a
+          todas las funciones del sitio.
+        </p>
+        <b-form class="my-5" @submit.stop.prevent="onSubmitRegisterForm">
+          <b-form-group
+            id="input-group-email"
+            label="Email:"
+            label-for="input-email"
+          >
+            <b-form-input
+              id="input-email"
+              v-model="form.email"
+              type="email"
+              required
+            ></b-form-input>
+          </b-form-group>
 
-                  <b-form-group
-                    id="input-group-password"
-                    label="Password:"
-                    label-for="input-password"
-                  >
-                    <b-form-input
-                      id="input-password"
-                      v-model="form.password"
-                      type="password"
-                      required
-                    ></b-form-input>
-                  </b-form-group>
-                  <b-button type="submit" variant="primary">
-                    Registrar Usuario
-                  </b-button>
-                </b-form>
-                <p class="text-muted">
-                  Tal vez querías
-                  <NuxtLink to="/login">iniciar sesión</NuxtLink>?
-                </p>
-              </template>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+          <b-form-group
+            id="input-group-password"
+            label="Password:"
+            label-for="input-password"
+          >
+            <b-form-input
+              id="input-password"
+              v-model="form.password"
+              type="password"
+              required
+            ></b-form-input>
+          </b-form-group>
+          <b-button type="submit" variant="primary">
+            Registrar Usuario
+          </b-button>
+        </b-form>
+        <p class="text-muted">
+          Tal vez querías
+          <NuxtLink to="/login">iniciar sesión</NuxtLink>?
+        </p>
+      </template>
+    </ContainersForm>
   </div>
 </template>
 
