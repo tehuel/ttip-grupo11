8e971a8e23310728754f8c2670a9b26ed20d049c
octi14 <gonzalezoctavio5@gmail.com>
2021-11-12 11:43:52 -0300

    Favourite recipes

---
+++ server/routes/fav.route.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const FavController = require("../controllers/fav.controller");
+const paginationMiddleware = require("express-pagination-middleware");
+const FavPagination = paginationMiddleware({
+  sort: { default: "created" },
+});
+
+// "/user" endpoints
+router.get("/", FavPagination, FavController.getComments);
+router.post("/", FavController.add);
+
+module.exports = router;
